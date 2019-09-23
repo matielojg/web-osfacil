b7f095eafc723cbc8515da5bfcc092a9a23a6869
Andrew Walmir <andrewwalmir@gmail.com>
2019-09-23 19:58:49 -0300

    menu ativo

--- app/Helpers/isActive.php
+++ app/Helpers/isActive.php
@@ -5,6 +5,5 @@ if (!function_exists('isActive')) {
     function isActive($href, $class = 'active')
     {
         return $class = (strpos(Route::currentRouteName(), $href) === 0 ? $class : '');
-
     }
 }
