c3d93cea58aee6cf438b9422b97c357a83b4c52b
Andrew Walmir <andrewwalmir@gmail.com>
2019-10-29 00:04:04 -0300

    alteracao na dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Order;
+use App\SectorProvider;
 use App\User;
 use Illuminate\Support\Facades\DB;
 
@@ -32,74 +33,76 @@ class DashboardController extends Controller
         $orderMedium = Order::where('priority', '=', 'media')->count();
         $orderLow = Order::where('priority', '=', 'baixa')->count();
 
+        //Dashboard Funcionário
+        $ordersEmployee = Order::where('requester', auth()->user()->id)->get();
 
-//        $ordersSupervisor = DB::table('orders')
-//            ->join('users', 'orders.requester', '=', 'users.id')
-//            ->join('sectors', 'orders.sector_provider', '=', 'sectors.id')
-//            ->join('services', 'orders.service', '=', 'services.id')
-//            ->select('orders.*', 'services.name_service', 'sectors.name_sector', 'users.first_name',
-//                'users.last_name')
-//            ->orderBy('priority', 'desc')
-//            ->orderBy('created_at', 'asc')
-//            ->get();
-//
-//        $ordersTechnical = DB::table('orders')
-//            ->join('users', 'orders.requester', '=', 'users.id')
-//            ->join('sectors', 'orders.sector_provider', '=', 'sectors.id')
-//            ->join('services', 'orders.service', '=', 'services.id')
-//            ->select('orders.*', 'services.name_service', 'sectors.name_sector', 'users.first_name',
-//                'users.last_name')
-//            ->orderBy('priority', 'desc')
-//            ->orderBy('created_at', 'asc')
-//            ->where('orders.responsible', '=', auth()->user()->id)  //view técnico
-//            ->get();
-//
-//        $ordersEmployee = DB::table('orders')
-//            ->join('users', 'orders.requester', '=', 'users.id')
-//            ->join('sectors', 'orders.sector_provider', '=', 'sectors.id')
-//            ->join('services', 'orders.service', '=', 'services.id')
-//            ->select('orders.*', 'services.name_service', 'sectors.name_sector', 'users.first_name',
-//                'users.last_name')
-//            ->orderBy('priority', 'desc')
-//            ->orderBy('created_at', 'asc')
-//            ->where('orders.requester', '=', auth()->user()->id)
-//            ->get();
+        //Dashboard Técnico
+        $ordersTechnical = Order::where('responsible', '=', auth()->user()->id)
+            ->whereIn('status', ['atribuido', 'em execucao'])
+            ->get();
 
-        $ordersEmployee = Order::where('requester','=', auth()->user()->id);
-        $ordersTechnical = Order::where('responsible','=', auth()->user()->id);
-        $ordersSupervisor = Order::all();
+        //Dashboard Supervisor
+        $sectorProviders = SectorProvider::where('supervisor', auth()->user()->id)
+            ->pluck('id');
+        $ordersSupervisor = Order::whereIn('sector_provider', $sectorProviders)
+            ->take(5)
+            ->latest()
+            ->get();
+
+        //Dashboard Gerente
+        $ordersManager = Order::whereNull('closed_at')
+            ->take(5)
+            ->latest()
+            ->get();
+
         $userFunction = auth()->user()->function;
 
-        //$json['message'] = $this->message->success('Seja Bem-Vindo!')->render();
-
-        if ($userFunction == 'funcionario') {
-            return view('admin.dashboard.dashboardEmployee', [
-                'orders' => $ordersEmployee
-            ]);
-        } elseif ($userFunction == 'tecnico') {
-            return view('admin.dashboard.dashboardTechnician', [
-                'orders' => $ordersTechnical
-            ]);
-        } else {
-
-            return view('admin.dashboard', [
-                'employee' => $employee,
-                'technician' => $technician,
-                'supervisor' => $supervisor,
-                'manager' => $manager,
-                'orderOpen' => $orderOpen,
-                'orderAssign' => $orderAssign,
-                'orderPending' => $orderPending,
-                'orderInExecution' => $orderInExecution,
-                'orderExecution' => $orderExecution,
-                'orderCritical' => $orderCritical,
-                'orderHigh' => $orderHigh,
-                'orderMedium' => $orderMedium,
-                'orderLow' => $orderLow,
-                'orders' => $ordersSupervisor
-
-            ]);
+        switch ($userFunction) {
+            case('gerente');
+                return view('admin.dashboard', [
+                    'employee' => $employee,
+                    'technician' => $technician,
+                    'supervisor' => $supervisor,
+                    'manager' => $manager,
+                    'orderOpen' => $orderOpen,
+                    'orderAssign' => $orderAssign,
+                    'orderPending' => $orderPending,
+                    'orderInExecution' => $orderInExecution,
+                    'orderExecution' => $orderExecution,
+                    'orderCritical' => $orderCritical,
+                    'orderHigh' => $orderHigh,
+                    'orderMedium' => $orderMedium,
+                    'orderLow' => $orderLow,
+                    'orders' => $ordersManager
+                ]);
+                break;
+            case('supervisor');
+                return view('admin.dashboard', [
+                    'employee' => $employee,
+                    'technician' => $technician,
+                    'supervisor' => $supervisor,
+                    'manager' => $manager,
+                    'orderOpen' => $orderOpen,
+                    'orderAssign' => $orderAssign,
+                    'orderPending' => $orderPending,
+                    'orderInExecution' => $orderInExecution,
+                    'orderExecution' => $orderExecution,
+                    'orderCritical' => $orderCritical,
+                    'orderHigh' => $orderHigh,
+                    'orderMedium' => $orderMedium,
+                    'orderLow' => $orderLow,
+                    'orders' => $ordersSupervisor
+                ]);
+            case('tecnico');
+                return view('admin.dashboard.dashboardTechnician', [
+                    'orders' => $ordersTechnical
+                ]);
+                break;
+            default;
+                return view('admin.dashboard.dashboardEmployee', [
+                    'orders' => $ordersEmployee
+                ]);
+                break;
         }
-
     }
 }
--- resources/views/admin/dashboard/dashboardTechnician.blade.php
+++ resources/views/admin/dashboard/dashboardTechnician.blade.php
@@ -4,7 +4,7 @@
 
     <div style="flex-basis: 100%;">
 
-        <section class="dash_content_app" style="margin-top: 40px;">
+        <section class="dash_content_app" style="margin-top: 10px;">
             <header class="dash_content_app_header">
                 <h2 class="icon-tachometer">Serviços a Realizar</h2>
             </header>
@@ -16,7 +16,7 @@
                         <tr>
                             <th>#</th>
                             <th>Solicitante</th>
-                            <th>Setor</th>
+                            <th>Setor Solicitante</th>
                             <th>Prioridade</th>
                             <th>Status</th>
                             <th>Data de Abertura</th>
@@ -29,23 +29,24 @@
 
                             <tr>
                                 <td> #{{$order->id}}</td>
-                                <td><a href="{{ route('admin.orders.edit', ['id'=>$order->id]) }}"
-                                       class="text-green">{{ $order->first_name }} {{ $order->last_name }}</a></td>
-
-                                <td><a href="{{ route('admin.orders.edit', ['id'=>$order->id]) }}"
-                                       class="text-green"> {{$order->name_sector}}</a>
+                                <td>
+                                    <a class="text-green">{{ $order->userRequester->first_name }} {{ $order->userRequester->last_name }}</a>
                                 </td>
-
-                                <td><a href="{{ route('admin.orders.edit', ['id'=>$order->id]) }}"
-                                       class="text-green">{{ ucfirst($order->priority) }}</a></td>
-                                <td><a href="{{ route('admin.orders.edit', ['id'=>$order->id]) }}"
-                                       class="text-green">{{ ucfirst($order->status) }}</a></td>
-                                <td><a href="{{ route('admin.orders.edit', ['id'=>$order->id]) }}"
-                                       class="text-green"> {{ date('d/m/Y H:i', strtotime($order->created_at))}}</a>
+                                <td><a class="text-green"> {{$order->sectorRequester->name_sector}}</a>
                                 </td>
-                                <td><a href="{{ route('admin.orders.edit', ['id'=>$order->id]) }}"
-                                       class="btn btn-green ml-1 icon-check-square-o">Editar</a></td>
-                                {{--                            <td><a href="{{ route('admin.orders.edit', ['id'=>$order->id]) }}" class="text-green"> {{ ( $sector->id == $order->sector_provider_id) ? 'selected' : '' }} > {{ $sector->name_sector }}</a></td>--}}
+                                <td><a class="text-green">{{ ucfirst($order->priority) }}</a></td>
+                                <td><a class="text-green">{{ ucfirst($order->status) }}</a></td>
+                                <td><a class="text-green"> {{ date('d/m/Y H:i', strtotime($order->created_at))}}</a>
+                                </td>
+                                <td>
+                                    @if($order->status == 'aberto' && $order->requester == auth()->user()->id)
+                                        <a href="{{ route('admin.orders.edit.open', ['id'=>$order->id]) }}"
+                                           class="btn btn-green ml-1 icon-pencil">Editar</a>
+                                    @else
+                                        <a href="{{ route('admin.orders.show', ['id'=>$order->id]) }}"
+                                           class="btn btn-green ml-1 icon-eye">Ver</a>
+                                    @endif
+                                </td>
                             </tr>
                         @endforeach
                         </tbody>
